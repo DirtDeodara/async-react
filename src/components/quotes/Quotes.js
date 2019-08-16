76e6f4ff998591f30a3b288a79914ade1fdad3d1
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-16 13:32:15 -0700

    created Quotes comp

---
+++ src/components/quotes/Quotes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Quote from '../quotes/Quote';
+
+function Quotes({ quotes }) {
+  const quoteElements = quotes.map(({ image, name, quote }) => {
+    <li key={quote}>
+      <Quote
+        image={image}
+        quote={quote}
+        name={name}
+      />
+    </li>;
+  });
+
+  return (
+    <ul>
+      {quoteElements}
+    </ul>
+  );
+}
+
+Quotes.propTypes = {
+  quotes: PropTypes.arrayOf(PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    quote: PropTypes.string.isRequired
+  }))
+};
+
+export default Quotes;
