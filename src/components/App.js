b585f94f0470f15e1fe5c60ede5e2d8eb4d95c31
Dirt Deodara <thecrowleaps@gmail.com>
2019-08-16 15:01:28 -0700

    made a form so that a user can choose how many quotes to fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import QuotesContainer from '../container/QuotesContainer';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+export default class App extends Component {
+  state = {
+    inputCount: '',
+    count: 5
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState(state => ({
+      count: state.inputCount,
+      inputCount: ''
+    }));
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ inputCount: Number.parseInt(target.value) });
+  }
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <input type="number" value={this.state.inputCount} onChange={this.handleChange}></input>
+          <button>FETCH</button>
+        </form>
+        <QuotesContainer count={this.state.count}/>
+      </>
+    );
+  }
 }
   
--- src/container/QuotesContainer.js
+++ src/container/QuotesContainer.js
@@ -9,21 +9,32 @@ export default class QuotesContainer extends Component {
   }
   state = {
     error: null,
+    loading: true,
     quotes: []
   }
 
   fetchQuotes = () => {
     return getQuotes(this.props.count)
       .then(quotes => {
-        this.setState({ quotes });
+        this.setState({ quotes, loading: false, error: null });
       })
-      .catch(err => this.setState({ error: err }));
+      .catch(err => this.setState({ error: err, loading: false }));
   }
 
   componentDidMount() {
     this.fetchQuotes();
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevProps.count !== this.props.count) {
+      this.setState({ loading: true });
+      this.fetchQuotes();
+    }
+  }
+
   render() {
+    if(this.state.error) return <h2>{this.state.error.message}</h2>;
+    if(this.state.loading) return <h1>Shiz be loading</h1>;
     return (
       <Quotes quotes={this.state.quotes}/>
     );
